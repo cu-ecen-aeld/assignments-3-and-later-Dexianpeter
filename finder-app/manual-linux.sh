df743a1b8de08783443b8c3d47d3679021850be7
Dexianpeter <dexianpeter@gmail.com>
2026-06-07 22:45:34 +0800

    fix bug and complete assignments3-part2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -15,30 +15,27 @@ CROSS_COMPILE=aarch64-linux-gnu-
 
 if [ $# -lt 1 ]
 then
-	echo "Using default directory ${OUTDIR} for output"
+    echo "Using default directory ${OUTDIR} for output"
 else
-	OUTDIR=$1
-	echo "Using passed directory ${OUTDIR} for output"
+    OUTDIR=$1
+    echo "Using passed directory ${OUTDIR} for output"
 fi
 
-OUTDIR=$(realpath "${OUTDIR}")
 mkdir -p ${OUTDIR}
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
-    #Clone only if the repository does not exist.
-	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
-	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
+    echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
+    git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
 fi
+
 if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
-    echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
-    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
@@ -49,11 +46,10 @@ echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
 then
-	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-    sudo rm  -rf ${OUTDIR}/rootfs
+    echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
+    sudo rm -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
 mkdir -p "${OUTDIR}/rootfs"
 cd "${OUTDIR}/rootfs"
 mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
@@ -66,67 +62,59 @@ then
     git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
     make distclean
     make defconfig
+    sed -i 's/# CONFIG_STATIC is not set/CONFIG_STATIC=y/' .config
+    sed -i 's/CONFIG_TC=y/# CONFIG_TC is not set/' .config
 else
     cd busybox
+    make distclean
+    make defconfig
+    sed -i 's/# CONFIG_STATIC is not set/CONFIG_STATIC=y/' .config
+    sed -i 's/CONFIG_TC=y/# CONFIG_TC is not set/' .config
 fi
 
-make distclean
-make defconfig
-sed -i 's/CONFIG_TC=y/CONFIG_TC=n/' .config
-sed -i 's/CONFIG_FEATURE_TC_INGRESS=y/CONFIG_FEATURE_TC_INGRESS=n/' .config
-sed -i 's/# CONFIG_STATIC is not set/CONFIG_STATIC=y/' .config
-
-# TODO: Make and install busybox
-make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CONFIG_PREFIX="${OUTDIR}/rootfs" install
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX="${OUTDIR}/rootfs" ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 echo "Library dependencies"
 cd "${OUTDIR}/rootfs"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter" || true
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library" || true
 
-# TODO: Add library dependencies to rootfs
-mkdir -p lib lib64
 SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
-if [ -z "${SYSROOT}" ] || [ "${SYSROOT}" = "/" ]; then
+if [ -z "$SYSROOT" ] || [ "$SYSROOT" = "/" ]; then
     SYSROOT="/usr/aarch64-linux-gnu"
 fi
 
-cp -a ${SYSROOT}/lib/ld-linux-aarch64.so.* lib/
-cp -a ${SYSROOT}/lib/libm.so.* lib64/
-cp -a ${SYSROOT}/lib/libresolv.so.* lib64/
-cp -a ${SYSROOT}/lib/libc.so.* lib64/
+cp -a ${SYSROOT}/lib/ld-linux-aarch64.so.* lib/ || true
+cp -a ${SYSROOT}/lib/libm.so.* lib64/ || true
+cp -a ${SYSROOT}/lib/libresolv.so.* lib64/ || true
+cp -a ${SYSROOT}/lib/libc.so.* lib64/ || true
 
-# TODO: Make device nodes
 mkdir -p dev
 sudo mknod -m 666 dev/null c 1 3
 sudo mknod -m 600 dev/console c 5 1
 
-# TODO: Clean and build the writer utility
 cd ${FINDER_APP_DIR}
 make clean
 make CROSS_COMPILE=${CROSS_COMPILE} CFLAGS="-O2 -g -static" LDFLAGS="-static"
-# TODO: Copy the finder related scripts and executables to the /home directory
-# on the target rootfs
+
 mkdir -p "${OUTDIR}/rootfs/home"
-cp writer "${OUTDIR}/rootfs/home/"
-cp finder.sh "${OUTDIR}/rootfs/home/"
-cp finder-test.sh "${OUTDIR}/rootfs/home/"
-cp autorun-qemu.sh "${OUTDIR}/rootfs/home/"
+cp writer finder.sh finder-test.sh autorun-qemu.sh "${OUTDIR}/rootfs/home/"
 
 mkdir -p "${OUTDIR}/rootfs/home/conf"
 cp ${FINDER_APP_DIR}/../conf/username.txt "${OUTDIR}/rootfs/home/conf/"
 cp ${FINDER_APP_DIR}/../conf/assignment.txt "${OUTDIR}/rootfs/home/conf/"
 
-sed -i 's/\.\.\/conf/conf/g' "${OUTDIR}/rootfs/home/finder-test.sh"
+cd "${OUTDIR}/rootfs/home"
+sed -i 's/\r//g' finder.sh finder-test.sh autorun-qemu.sh
+sed -i 's/\.\.\/conf\/assignment\.txt/conf\/assignment\.txt/g' finder-test.sh
+chmod +x finder.sh finder-test.sh autorun-qemu.sh writer
 
-# TODO: Chown the root directory
 cd "${OUTDIR}/rootfs"
 sudo chown -R root:root *
 
-# TODO: Create initramfs.cpio.gz
-find . | cpio -H newc -ov --owner root:root | gzip -9 > "${OUTDIR}/initramfs.cpio.gz"
-
-echo "BUILD SUCCESSFUL!"
+find . | cpio -H newc -ov --owner root:root > "${OUTDIR}/initramfs.cpio"
+cd "${OUTDIR}"
+gzip -f initramfs.cpio
